c6c2bb4cba8c7053ac2dae49a21398ababb0c0a4
b1018168 <b1018168@10.124.48.34>
2023-01-19 15:58:56 +0900

    Test Test Test

--- src/main/java/com/example/demo/JudgeUtils.java
+++ src/main/java/com/example/demo/JudgeUtils.java
@@ -7,7 +7,8 @@ public class JudgeUtils extends MiraiPrjApplication {
 
 	// 定数
 	public static final int AGE = 17;
-
+	public static final int KAKINAGE = 18;
+	
 	/**
 	 * 課金対象か判定するメソッド<br>
 	 *
@@ -26,7 +27,7 @@ public class JudgeUtils extends MiraiPrjApplication {
 		// 現在の年月日のみをセット
 		compareCal.set(curYear, curMonth, curDate);
 		// 年からプレイ可能年齢を引く
-		compareCal.add(Calendar.YEAR, -AGE);
+		compareCal.add(Calendar.YEAR, -KAKINAGE);
 
 		// 生年月日を設定
 		Calendar birthCal = Calendar.getInstance();
